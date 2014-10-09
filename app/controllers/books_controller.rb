349dc24cfdbb1c6f29a277dd644542870c469792
David Fisher <tibbon@gmail.com>
2014-10-09 11:39:42 -0400

    Two ways of submitting new books, both without nested routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,10 @@ class BooksController < ApplicationController
     redirect_to library_path(book.library)
   end
 
+  def new
+    @book = Book.new(library_id: params[:library_id])
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :library_id)
