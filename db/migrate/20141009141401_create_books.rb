12a99c637eb97f9700a342e56f3d9f58818645ec
David Fisher <tibbon@gmail.com>
2014-10-09 10:16:15 -0400

    Fixed a bad migration and created library and book models

--- db/migrate/20141009141401_create_books.rb
+++ db/migrate/20141009141401_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :title
-      t.belong_to :library
+      t.belongs_to :library, index: true
     end
   end
 end
