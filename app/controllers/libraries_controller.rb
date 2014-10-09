b9421e8aa7c8750913c91a39fcc87cc7c0c07a7e
David Fisher <tibbon@gmail.com>
2014-10-09 10:38:31 -0400

    Updated some examples in libraries controller and filled in index

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,6 +1,11 @@
+# When a req is made to /libraries GET
+# Router says "Make a new instance of a LibrariesController
+# and then execute the index method/action"
+
 class LibrariesController < ApplicationController
   def index
     @libraries = Library.all
+    # render(*args)
   end
 
   def show
